58a5f7c7d8c22cf951c267973affbcaeb440adc9
Anton <antonio.fiasco24@gmail.com>
2023-11-20 14:23:53 +0000

    finished task

--- app.js
+++ app.js
@@ -6,14 +6,29 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'static')))
 
 app.get('/stocks', async (req, res) => {
-  const stockSymbols = await stocks.getStocks()
-  res.send({ stockSymbols })
+  try {
+    const stockSymbols = await stocks.getStocks()
+    res.send({ stockSymbols })
+  }
+  catch (error) {
+    console.error('Error fetching stock symbols: ', error.message);
+    res.status(500).send({ error: error.message });
+  }
 })
 
 app.get('/stocks/:symbol', async (req, res) => {
-  const { params: { symbol } } = req
-  const data = await stocks.getStockPoints(symbol, new Date())
-  res.send(data)
+  try {
+    const { params: { symbol } } = req
+    const data = await stocks.getStockPoints(symbol, new Date())
+    res.send(data)
+  }
+  catch (error) {
+    console.error('Error while fetching stock points: ', error.message);
+
+    // Ideally status code should be 404 when the symbol is not found but as I can't change stock.js
+    // there's no proper way of doing it because error type is the same for both errors.
+    res.status(500).send([])
+  }
 })
 
 app.listen(3000, () => console.log('Server is running!'))
--- static/scripts.js
+++ static/scripts.js
@@ -1,6 +1,121 @@
 const canvas = document.getElementById('chart')
 const ctx = canvas.getContext('2d')
 
+async function getStockData() {
+  try {
+    const response = await fetch('/stocks');
+    const data = await response.json();
+
+    const stockDetails = await getStockDetails(data.stockSymbols);
+
+    const symbolsDiv = document.getElementById('symbols');
+    data.stockSymbols.forEach(item => {
+      const button = document.createElement('button');
+      button.textContent = item;
+      button.onclick = () => drawChart(stockDetails[item]);
+      button.disabled = stockDetails[item].length === 0;
+      symbolsDiv.appendChild(button);
+    })
+  }
+  catch (error) {
+    console.error('Error fetching stock symbols:', error);
+    const errorDiv = document.createElement('div');
+    errorDiv.innerText = 'Error fetching stock symbols';
+    errorDiv.style.color = 'red';
+    errorDiv.style.fontSize = '28px';
+    document.body.appendChild(errorDiv);
+  }
+  finally {
+    // Remove loading spinner
+    const spinner = document.getElementById('spinner');
+    spinner.remove();
+  }
+}
+
+// Fetch data on page load
+window.onload = getStockData;
+
+async function getStockDetails(stockSymbols) {
+  // Fetch all symbols using Promise.all
+  const promise = Promise.all(
+      stockSymbols.map(symbol => {
+        return fetch(`/stocks/${symbol}`)
+            .then(res => res.json())
+      })
+  )
+
+  const data = await promise;
+
+  // Format data into an object of type Record<string, array>
+  let result = {}
+  for (let i = 0; i < stockSymbols.length; i++) {
+    result[stockSymbols[i]] = data[i];
+  }
+  console.log(result);
+
+  return result;
+}
+
+// Scales value from stock values range to Y axis range
+function scaleValue(value, oldMin, oldMax, newMin, newMax) {
+  return newMax - ((value - oldMin) / (oldMax - oldMin)) * (newMax - newMin);
+}
+
+// Draws base of the chart
+function drawBase() {
+  // Clears chart before drawing axes
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawLine([50, 50], [50, 550])
+  drawTriangle([35, 50], [65, 50], [50, 35])
+
+  drawLine([50, 550], [950, 550])
+  drawTriangle([950, 535], [950, 565], [965, 550])
+}
+
+function drawChart(stockDetails) {
+  // const sortedStockDetails = stockDetails.sort((a, b) => a.timestamp - b.timestamp)
+
+  drawBase();
+
+  const minStockValue = Math.min(...stockDetails.map(item => item.value));
+  const maxStockValue = Math.max(...stockDetails.map(item => item.value));
+
+  const minY = 100;
+  const maxY = 500;
+
+  const minX  = 100;
+  const maxX = 900;
+
+  const stepX = (maxX - minX) / stockDetails.length;
+
+  let x = minX;
+
+  for (let i = 0; i < stockDetails.length - 1; i++) {
+    const dateTime = new Date(stockDetails[i].timestamp);
+    const formattedTime = `${dateTime.getHours()}:${dateTime.getMinutes()}`;
+
+    const startValue = stockDetails[i].value;
+    const endValue = stockDetails[i + 1].value;
+
+    const startX = x;
+    const endX = startX + stepX;
+
+    const startY = scaleValue(startValue, minStockValue, maxStockValue, minY, maxY);
+    const endY = scaleValue(endValue, minStockValue, maxStockValue, minY, maxY);
+
+    console.log(startX, endX, startY, endY)
+
+    drawLine([startX, startY], [endX, endY], 'blue');
+
+    // Add labels for the X and Y axes
+    ctx.fillStyle = 'black';
+    ctx.fillText(formattedTime, startX, maxY + 20); // X axis label
+    ctx.fillText(startValue.toFixed(2), minX - 40, startY); // Y axis label
+    x += stepX;
+  }
+}
+
 function drawLine (start, end, style) {
   ctx.beginPath()
   ctx.strokeStyle = style || 'black'
@@ -16,9 +131,3 @@ function drawTriangle (apex1, apex2, apex3) {
   ctx.lineTo(...apex3)
   ctx.fill()
 }
-
-drawLine([50, 50], [50, 550])
-drawTriangle([35, 50], [65, 50], [50, 35])
-
-drawLine([50, 550], [950, 550])
-drawTriangle([950, 535], [950, 565], [965, 550])
